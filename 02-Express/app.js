9b99225ca47020de548761796d15036502cbe58c
shubham <shubhamsagar628@gmail.com>
2024-07-03 19:03:07 +0530

    Connect Flash Middlware use

--- 02-Express/app.js
+++ 02-Express/app.js
@@ -4,31 +4,52 @@ const app = express()
 
 
 const expressSession = require('express-session')
-
+const flash = require('connect-flash')
 
 
 // Middleware
+// 1)---------------------------------Express-Session-----------------
+// app.use(expressSession({
+//     secret:"random secret",
+//     resave:false,
+//     saveUninitialized:false
+// }))
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello Backend")
+// })
+
+
+ 
+// app.get("/create",function(req,res,next){
+//     req.session.polo ="true"
+//     res.send("Done")
+// })
+
+// app.get("/check",(req,res)=>{
+//   console.log(req.session.polo)   
+  
+//})
+
+
 app.use(expressSession({
-    secret:"random secret",
+    secret:"abc",
     resave:false,
     saveUninitialized:false
 }))
 
-app.get("/",(req,res)=>{
-    res.send("Hello Backend")
+app.use(flash())
+
+app.get('/',function(req,res,next){
+    req.flash("error","Invalid Crededntials")
+    res.redirect('/error')
 })
 
-
- 
-app.get("/create",function(req,res,next){
-    req.session.polo ="true"
-    res.send("Done")
+app.get('/error',function(req,res,next){
+    const messg = req.flash('error')
+    res.send(messg)
 })
 
-app.get("/check",(req,res)=>{
-  console.log(req.session.polo)   
-  
-})
 
 app.listen(3000,()=>{
     console.log("Server is Running on Port 3000")
