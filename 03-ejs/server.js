86836466a3ce393b2f52b6a3e5c2471a8dfe1aba
shubham <shubhamsagar628@gmail.com>
2024-07-05 19:17:23 +0530

    Form in ejs

--- 03-ejs/server.js
+++ 03-ejs/server.js
@@ -8,9 +8,19 @@ app.get("/",(req,res)=>{
     res.send("Backend Working")
 })
 
-app.get("/temp",function(req,res){
-    res.render("index")
+// app.get("/temp",function(req,res){
+//     res.render("index")
+// })
+
+app.get("/getForm",(req,res)=>{
+    res.render('form')
 })
+
+app.get("/check",(req,res)=>{
+    res.send("All Things are Good")
+    console.log(req.query)
+})
+
 
 app.listen(3000,(req,res)=>{
     console.log('Server is running on port 3000')
